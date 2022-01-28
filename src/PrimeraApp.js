63ed16bbc4997a11d987a4e1d376acd2e1576735
Pablo Arce <arcepablo07@gmail.com>
2022-01-28 14:57:35 -0300

    component props (#2801)

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,7 +2,9 @@ import React from "react";
 
 // Functional Components - Componentes basados en constantes
 
-const PrimeraApp = () => {
+// Normalmente se desestructuran las props para utilizarlas
+
+const PrimeraApp = ({ parentProp, propDefault = 'Default Value'}) => {
     
     const texto = 'Etiqueta h2 desde una constante'
 
@@ -11,6 +13,8 @@ const PrimeraApp = () => {
         <h1> Etiqueta h1 desde una Functional Component </h1>
         <h2> {texto} </h2>
         <p> Parrafo incluido en un fragment</p>
+        <p>Aquí se muestra el valor obtenido desde: " {parentProp} "</p>
+        <p>En este parrafo se puede ver el valor por defecto de una propiedad del componente PrimeraApp incluida en la desestructuración de sus props: " {propDefault} " </p>
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <PrimeraApp />, divRoot );
+ReactDOM.render( <PrimeraApp parentProp = 'Propiedad desde el elemento padre'/>, divRoot );
