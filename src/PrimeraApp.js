13b960505e3a78be888087071ac6324cdc998d2d
Pablo Arce <arcepablo07@gmail.com>
2022-01-28 11:27:09 -0300

    functional components (#2801)

---
+++ src/PrimeraApp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// Functional Components - Componentes basados en constantes
+
+const PrimeraApp = () => {
+    return <h1> Etiqueta h1 desde una Functional Component </h1>
+}
+
+export default PrimeraApp;
