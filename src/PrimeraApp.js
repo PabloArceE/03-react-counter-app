28ceaad44211323c4de39c7f17aac6efe87ba95d
Pablo Arce <arcepablo07@gmail.com>
2022-01-28 11:40:17 -0300

    fragments (#2801)

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -3,7 +3,12 @@ import React from "react";
 // Functional Components - Componentes basados en constantes
 
 const PrimeraApp = () => {
-    return <h1> Etiqueta h1 desde una Functional Component </h1>
+    return (
+        <>
+        <h1> Etiqueta h1 desde una Functional Component </h1>
+        <p> Parrafo incluido en un fragment</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
