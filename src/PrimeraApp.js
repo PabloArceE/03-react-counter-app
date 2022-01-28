ff96dc0bd221e0e602ea4298c58e39c4a61f7ac6
Pablo Arce <arcepablo07@gmail.com>
2022-01-28 11:48:31 -0300

    uso de constantes en un fragment (#2801)

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -3,9 +3,13 @@ import React from "react";
 // Functional Components - Componentes basados en constantes
 
 const PrimeraApp = () => {
+    
+    const texto = 'Etiqueta h2 desde una constante'
+
     return (
         <>
         <h1> Etiqueta h1 desde una Functional Component </h1>
+        <h2> {texto} </h2>
         <p> Parrafo incluido en un fragment</p>
         </>
     );
